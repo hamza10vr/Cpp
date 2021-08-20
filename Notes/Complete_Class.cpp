ffc4e1dfc231c3840b753b5dad20dfd821fd4011
Group 7 <hxtanveer@shockers.wichita.edu>
2021-08-20 00:26:20 -0500

    workingclass

--- Notes/Complete_Class.cpp
+++ Notes/Complete_Class.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+#include <string>
+
+class Restaurant {
+	public:
+		void SetName(std::string restaurantName) { this->name = restaurantName; }
+		void SetRating(int userRating) { this->rating = userRating; }
+		void print();
+
+	private:
+		std::string name;
+		int rating;
+
+};
+
+int main() {
+
+	std::cout << "hello world\n";
+
+	return 0;
+};
+
